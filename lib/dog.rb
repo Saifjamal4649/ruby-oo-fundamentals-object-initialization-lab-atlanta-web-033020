9281630194a2699fb221eb43c927a85838d7865d
Saif Jamal <saifjamal4649@gmail.com>
2020-03-31 21:53:07 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,12 @@
 class Dog 
   def initialize(name, breed)
     @name = name 
-    @breed = breed 
-    @breed = "Mutt"
   end 
+  def breed=(breed)
+    @breed = breed
+  end
+ 
+  def breed
+    @breed
+  end
 end 
